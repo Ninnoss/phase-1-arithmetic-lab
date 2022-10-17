0159c2c58a2bf03ce032912f5672ce68c1b0c6e8
Ninos Dinkha <ninos.dinkha97@gmail.com>
2022-10-18 00:06:18 +0300

    Assignmet done!

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+// Multiply test
+let num1 = 62, num2 = 1;
+const multiply = num1 * num2
+
+// random test
+const random = Math.floor(Math.random() * 10) + 1;
+
+// mod test
+let num3 = 100, num4 = 24;
+const mod = num3 % num4;
+
+// max test
+let nums = [1, 4, 7, 15, 20]
+const max = Math.max(...nums)
